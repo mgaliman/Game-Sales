b70631f9379d1af15fe6fa9fc9b17f4938bd00b0
Marko <61890185+mgaliman@users.noreply.github.com>
2020-06-18 23:38:13 +0200

    Counter update

--- Project/quick_sort.cpp
+++ Project/quick_sort.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include "quick_sort.h"
 using namespace std;
 
+int countt = 0;
 void quick_sort(vector<VideoGame>& data, int left, int right) {
+	
+	countt++;
 	if (right <= left) {
+		countt--;
 		return;
 	}
 
@@ -24,12 +29,22 @@ void quick_sort(vector<VideoGame>& data, int left, int right) {
 			swap(data[i], data[j]);
 		}
 	}
-
+	
 	swap(pivot, data[j]);
 	quick_sort(data, left, j - 1);
 	quick_sort(data, j + 1, right);
+	
 }
 
 void quick_sort(vector<VideoGame>& data) {
 	quick_sort(data, 0, data.size() - 1);
+	ofstream out("Counter.txt");
+	if (!out)
+		cout << "ERROR::Counter.txt";
+
+	for (int i = 0; i < countt; i++)
+	{
+		out << '#';
+	}
+	out.close();
 }
--- Project/quick_sort.h
+++ Project/quick_sort.h
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+
 void quick_sort(vector<VideoGame>& data);
 
 #endif
